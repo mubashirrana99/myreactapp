79299c887feeaa4c61cf1104bbc6b9b90e971e67
mubashirrana99 <60287983+mubashirrana99@users.noreply.github.com>
2021-02-05 02:36:59 -0800

    change

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+const express=require ("express");
+const app=express();
+const port=8000;
+
+app.get("/",(req,res)=>{
+    res.send("hello from express server")
+})
+
+app.listen(port,()=>{
+    console.log('listening to port $ {port}')
+})
